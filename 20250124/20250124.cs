11bc8341557462b65b6aed6b8b7bd5e1ed0111f3
kyungmin2322 <kyungmin2322@gmail.com>
2025-01-24 15:31:06 +0900

    20250124 15:30 Not Array

--- 20250124/20250124.cs
+++ 20250124/20250124.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
+            // 1 ~ 100의 홀수/짝수들의 합
+            int iEvenSum = 0;
+            int iOddSum = 0;
 
+            for(int i = 1; i <= 100; i++)
+            {
+                if(i % 2 == 1)
+                {
+                    iOddSum += i;
+                }
+                else if(i % 2 == 0)
+                {
+                    iEvenSum += i;
+                }
+            }
+            Console.WriteLine(iOddSum);
+            Console.WriteLine(iEvenSum);
         }
     }
 }
