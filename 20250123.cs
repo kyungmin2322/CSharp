f6352d4e03021f5146101b4e375bb7eb67321f8b
kyungmin2322 <kyungmin2322@gmail.com>
2025-01-24 14:24:46 +0900

    Create 20250123.cs

---
+++ 20250123.cs
@@ -0,0 +1,11 @@
+﻿namespace C20250123
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+        }
+    }
+}
