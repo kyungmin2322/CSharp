3563681292999912c6d4e2c9bbc37ee2942686b1
kyungmin2322 <kyungmin2322@gmail.com>
2025-01-24 15:20:40 +0900

    20250124 15:18

--- 20250124/20250124.cs
+++ 20250124/20250124.cs
@@ -1,17 +1,10 @@
-﻿namespace C20250123
+﻿namespace C20250124
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int iKorean = 10;
-            int iMath = 100;
-            int iEnglish = 50;
 
-            int iTotal = iKorean + iMath + iEnglish;
-            float fAverage = iTotal / 3.0f;
-            Console.WriteLine(iTotal);
-            Console.WriteLine(fAverage);
         }
     }
 }
