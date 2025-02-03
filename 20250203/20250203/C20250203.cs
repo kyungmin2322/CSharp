1d6a64d06a43795fa9214f8fb3c347a1bb1871d5
kyungmin2322 <kyungmin2322@gmail.com>
2025-02-03 10:18:23 +0900

    20250203 10:18

---
+++ 20250203/20250203/C20250203.cs
@@ -0,0 +1,10 @@
+﻿namespace _20250203
+{
+    internal class C20250203
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
