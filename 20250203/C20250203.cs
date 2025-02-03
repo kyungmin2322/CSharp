2435fa3f8b8094145e1cf1210d324d021187f569
kyungmin2322 <kyungmin2322@gmail.com>
2025-02-03 16:34:11 +0900

    20250203 16:33 숫자 2개를 받아서 곱한 후 리턴하는 함수

--- 20250203/C20250203.cs
+++ 20250203/C20250203.cs
@@ -2,36 +2,15 @@
 {
     internal class C20250203
     {
-        static int iSize = 10;
-        static int[,] iData = new int[iSize, iSize];
-        static void Initialze()
+        static int Multiply(int x, int y)
         {
-
-            for (int i = 0; i < iSize; i++)
-            {
-                for (int j = 0; j < iSize; j++)
-                {
-                    iData[i, j] = (i * 10) + j + 1;
-                }
-            }
-        }
-        static void Print()
-        {
-            for (int i = 0; i < iSize; i++)
-            {
-                for (int j = 0; j < iSize; j++)
-                {
-                    Console.Write(iData[i, j] + "\t");
-                }
-                Console.WriteLine();
-            }
+            return x * y;
         }
 
 
         static void Main(string[] args)
         {
-            Initialze();
-            Print();
+            Console.WriteLine(Multiply(2, 3));
         }
     }
 }
