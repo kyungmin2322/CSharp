f61719b5f402d2aaa2fcdfba8eef0c832b2ffdae
kyungmin2322 <kyungmin2322@gmail.com>
2025-02-03 16:09:53 +0900

    20250203 16:09 함수 사용

--- 20250203/C20250203.cs
+++ 20250203/C20250203.cs
@@ -2,26 +2,36 @@
 {
     internal class C20250203
     {
-        static void Main(string[] args)
+        static int iSize = 10;
+        static int[,] iData = new int[iSize, iSize];
+        static void Initialze()
         {
-            int iSize = 10;
-            int[,] iData = new int[iSize, iSize];
 
-            for(int i = 0; i < iSize; i++)
+            for (int i = 0; i < iSize; i++)
             {
-                for(int j = 0; j < iSize; j++)
+                for (int j = 0; j < iSize; j++)
                 {
                     iData[i, j] = (i * 10) + j + 1;
                 }
             }
-            for(int i = 0; i < iSize; i++)
+        }
+        static void Print()
+        {
+            for (int i = 0; i < iSize; i++)
             {
-                for(int j = 0; j < iSize; j++)
+                for (int j = 0; j < iSize; j++)
                 {
                     Console.Write(iData[i, j] + "\t");
                 }
                 Console.WriteLine();
             }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Initialze();
+            Print();
         }
     }
 }
