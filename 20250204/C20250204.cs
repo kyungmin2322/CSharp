e36a4df4397739f2e79d53934a6a35597b6ae094
kyungmin2322 <kyungmin2322@gmail.com>
2025-02-04 09:33:45 +0900

    20250204 9:33 1-52까지 배열에 숫자를 넣고 임의의 숫자를 8개 출력하기

---
+++ 20250204/C20250204.cs
@@ -0,0 +1,29 @@
+﻿namespace _20250204
+{
+    internal class C20250204
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[52];
+            Random rand = new Random();
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                array[i] = i + 1;
+            }
+
+            for(int i = 0; i < 8; i++)
+            {
+                // 랜덤 값 생성
+                int arrayNum = rand.Next(i, array.Length);
+
+                Console.Write($"{array[arrayNum]}, ");
+
+                int temp = array[arrayNum];
+                array[arrayNum] = array[i];
+                array[i] = temp;
+            }
+
+        }
+    }
+}
