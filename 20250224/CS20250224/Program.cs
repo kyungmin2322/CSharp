b7d5e32fefdfdbd213c87a92c8b5826ec3033863
kyungmin2322 <kyungmin2322@gmail.com>
2025-02-24 12:23:48 +0900

    20250224 12:23 1-3교시 비트연산

---
+++ 20250224/CS20250224/Program.cs
@@ -0,0 +1,90 @@
+﻿namespace CS20250224
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			/* 1교시
+			int n = Convert.ToInt32(Console.ReadLine());
+
+			if ( n >= 1 && n <= 16 )
+			{
+				int [] arr1 = new int [n];
+				int [] arr2 = new int [n];
+
+				for ( int i = 0; i < n; i++ )
+				{
+					arr1 [i] = Convert.ToInt32(Console.ReadLine());
+				}
+				for ( int i = 0; i < n; i++ )
+				{
+					arr2 [i] = Convert.ToInt32(Console.ReadLine());
+				}
+
+				for ( int i = 0; i < n; i++ )
+				{
+					for(int j = 0; j < n; j++)
+					{
+						Console.Write((uint)( arr1 [i] | arr2 [i] ) > 0 ? '#' : ' ');
+					}
+					Console.WriteLine();
+				}
+			}
+			else return;
+			*/
+			/* 2교시
+			int n = 5;
+			int [] arr1 = { 9, 20, 28, 18, 11 };
+			int [] arr2 = { 30, 1, 21, 17, 28 };
+
+			// 첫번째 방법
+			for(int i = 0; i < n; i++)
+			{
+				Console.WriteLine(Convert.ToString(arr1 [i] | arr2 [i], 2)
+					.Replace('1', '#').Replace('0', ' '));
+			}
+			// 두번째 방법
+			// int bitmask = 0b00000001;
+			// for(int i = 0; i < n; i++) 
+			// {
+			//		bitmask = 1 << (n - 1);
+			//		for(int j = 0; j < n; j++)
+			//		{
+			//			Console.Write((bitmask & result[i]) > 0 ? "#" : " ");
+			//			bitmask = bitmask >> 1;
+			//		}
+			//		Console.Write(", ");
+			// }
+			*/
+			/* 3교시
+			uint N = 3;
+			uint.TryParse(Console.ReadLine(), out N);
+
+			ulong [] X = new ulong[N];
+
+			for(int i = 0; i < N; i++)
+			{
+				ulong.TryParse(Console.ReadLine(), out X[i]);
+			}
+
+			ulong result = 0;
+			for(int i = 0; i < N; i++)
+			{
+				ulong value = 1;
+				for(int n = 1; n < 64; n++)
+				{
+					value = value << 1;
+					if ( X [i] < value)
+					{
+						result = result ^ value;
+						break;
+					}
+				}
+			}
+			Console.WriteLine(result);
+			*/
+
+
+		}
+    }
+}
---
+++ 20250224/CS20250224/Quiz00.cs
@@ -0,0 +1,88 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace CS20250224
+//{
+//	public class Quiz00
+//	{
+//		BitArray32 bitArray = new BitArray32();
+//		// 0101
+//		bitArray.On(3);
+//            bitArray.On(1);
+//			Console.WriteLine(Convert.ToString(bitArray.Data, 2));
+
+//            bitArray.Off(3);
+//			Console.WriteLine(Convert.ToString(bitArray.Data, 2));
+
+
+//			// byte a = 0; // ==> 0000 0000
+//			// a = 1 << 1; // ==> 0000 0001 -> 0000 0010
+
+//			// << shift 연산자
+//			// >>
+
+//			// 0101
+//			// 0110 & (논리곱), and
+//			// 0100
+
+//			// 0011
+//			// 0101 | (논리합)
+//			// 0111
+
+//			// 0001 ~ (부정)
+//			// 1110
+
+//			// 0101
+//			// 0011 ^ (XOR)
+//			// 0110
+
+//			// 0000 0000 -> 16진수
+//			// F    F    -> Color
+//			// 0xFF
+//			// 255
+
+//			//byte Player = 1; // => 0b0000 0001
+//			//byte Camera = 2; // => 0b0000 0010
+//			//byte UI =     4; // => 0b0000 0100
+//			//byte Water =  8; // => 0b0000 1000
+
+//			//// Player
+//			//byte playerLayer = 0b00000000;
+//			//            // => 0b00000001 |
+//			//            // => 0b00000011
+//			//playerLayer = (byte)(playerLayer | Player);
+//			//playerLayer = (byte)(playerLayer | UI);
+
+//			//// bit masking
+//			//if( (playerLayer & (Camera | Player)) > (byte)0 )
+//			//{
+
+//			//}
+//	}
+//	class BitArray32
+//	{
+//		public uint Data;
+
+//		public void On( int position )
+//		{
+//			if ( position > 0 && position <= 32 )
+//			{
+//				Data = Data | (uint)( 1 << position - 1 );
+//			}
+//		}
+//		public void Off( int position )
+//		{
+//			if ( position > 0 && position <= 32 )
+//			{
+//				Data = Data & ~(uint)( 1 << position - 1 );
+//			}
+//		}
+//		public bool Check( uint other )
+//		{
+//			return (int)( Data & other ) > 0 ? true : false;
+//		}
+//	}
+//}
