1248cee3cde21dce62d687a6a92af9e66d60fb7b
kyungmin2322 <kyungmin2322@gmail.com>
2025-04-14 11:28:05 +0900

    20250414 11:27 백준 #10828

--- 20250414/CS20250414/Program.cs
+++ 20250414/CS20250414/Program.cs
@@ -1,28 +1,65 @@
-﻿namespace CS20250414
+﻿using System.Text;
+
+namespace CS20250414
 {
     class Program
     {
-        struct A
-        {
-
-        }
-
         static void Main(string[] args)
         {
-            Stack<int> st = new();
+			// 명령의 갯수 입력
+			int inNum = int.Parse(Console.ReadLine());
+			Stack<int> stack = new Stack<int>();
 
-            // push(삽입)
-            st.Push(1); // 1
-            st.Push(2); // 2 1
-            st.Push(3); // 3 2 1
+			// 명령 종료 조건
+			for(int i = 0; i < inNum; i++)
+			{
+				string input = Console.ReadLine();
+				string[] pushing = input.Split(' ');
 
-			// peek(읽기)
-            int num2 = st.Peek(); // 3 | 3 2 1
+				StringBuilder sb = new StringBuilder();
 
-			// pop(삭제)
-			int num = st.Pop(); // 3 | 2 1
-            num = st.Pop(); // 2 | 1
-            num = st.Pop(); // 1 | empty
-        }
-    }
+				if(input.Contains("push"))
+				{
+					int pushNum = int.Parse(pushing[1]);
+					stack.Push(pushNum);
+				}
+				else if(input.Contains("pop"))
+				{
+					if(stack.Count == 0)
+					{
+						sb.Append(-1);
+					}
+					else
+					{
+						sb.Append(stack.Pop());
+					}
+				}
+				else if(input.Contains("size"))
+				{
+					sb.Append(stack.Count());
+				}
+				else if(input.Contains("empty"))
+				{
+					int empty = (stack.Count() > 0) ? 0 : 1;
+					sb.Append(empty);
+				}
+				else if(input.Contains("top"))
+				{
+					if(stack.Count == 0)
+					{
+						sb.Append(-1);
+					}
+					else
+					{
+						sb.Append(stack.Peek());
+					}
+				}
+
+				if(!input.Contains("push"))
+				{
+					Console.WriteLine(sb.ToString());
+				}
+			}
+		}
+	}
 }
