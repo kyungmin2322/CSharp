7576c7097a5e7ebd7332d07bf927d01aed495dd2
kyungmin2322 <kyungmin2322@gmail.com>
2025-04-14 09:52:17 +0900

    20250414 09:52 Stack(1)

---
+++ 20250414/CS20250414/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace CS20250414
+{
+    class Program
+    {
+        struct A
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            Stack<int> st = new();
+
+            // push(삽입)
+            st.Push(1); // 1
+            st.Push(2); // 2 1
+            st.Push(3); // 3 2 1
+
+			// peek(읽기)
+            int num2 = st.Peek(); // 3 | 3 2 1
+
+			// pop(삭제)
+			int num = st.Pop(); // 3 | 2 1
+            num = st.Pop(); // 2 | 1
+            num = st.Pop(); // 1 | empty
+        }
+    }
+}
---
+++ CS20250331/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace CS20250331
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StartPrintStar(5);
+        }
+        
+        static void StartPrintStar(int num)
+        {
+            // 기저 조건 : 끝내는 조건
+            if(num <= 0) // !(num < 5)
+            {
+                return;
+			}
+            StartPrintStar(num - 1);
+
+            PrintStarLine(num);
+			Console.WriteLine();
+        }
+
+        static void PrintStarLine(int starCount)
+        {
+            if(starCount <= 0)
+            {
+                return;
+            }
+            PrintStarLine(starCount - 1);
+
+			Console.Write('*');
+        }
+    }
+}
